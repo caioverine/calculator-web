11c94231279ea630eac60848a95c8786f267e96b
Caio Vinícius <viniciuscaio.17@gmail.com>
2024-09-14 19:10:33 -0300

    Add files via upload

---
+++ src/app/operation-form/operation-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operation-form',
+  templateUrl: './operation-form.component.html',
+  styleUrls: ['./operation-form.component.scss']
+})
+export class OperationFormComponent {
+
+}
